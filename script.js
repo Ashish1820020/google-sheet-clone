bb02676d14cc4f83b715f4cb4daf25522cad1e42
Ashish Bhattacharyya <ashishbhattacharyya@gmail.com>
2025-05-01 14:35:52 +0530

    added structure

---
+++ script.js
@@ -0,0 +1,42 @@
+const cols = 26;
+const rows = 100;
+
+const addressColCont = document.querySelector(".address-column-cont");
+const addressRowCont = document.querySelector(".address-row-cont");
+const cellCont = document.querySelector(".cell-cont");
+const addressBar = document.querySelector(".address-bar");
+
+for (let row = 1; row<=rows; row++) {
+    const colCell = document.createElement("div");
+    colCell.innerText = row;
+    colCell.setAttribute("class", "address-col");
+    addressColCont.appendChild(colCell)
+}
+
+for (let col = 0; col<cols; col++) {
+    const rowCell = document.createElement("div");
+    rowCell.innerText = String.fromCharCode(65 + col);
+    rowCell.setAttribute("class", "address-row");
+    addressRowCont.appendChild(rowCell)
+}
+
+for (let row = 0; row<rows; row++) {
+    const rowBar = document.createElement("div");
+    rowBar.setAttribute("class", "row-cont");
+    for (let col = 0; col<cols; col++) {
+        const cell = document.createElement("div");
+        cell.setAttribute("class", "cell");
+        cell.setAttribute("contenteditable", "true");
+        addClickListnerForCell(cell, row, col);
+        rowBar.appendChild(cell);
+    }
+    cellCont.appendChild(rowBar)
+};
+
+function addClickListnerForCell (cell, i, j) {
+    const colId = i+1;
+    const rowId = String.fromCharCode(65+j);
+    cell.addEventListener("click", () => {
+        addressBar.value = `${rowId}${colId}`
+    })
+}
